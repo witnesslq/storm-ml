a75c50d456aeebb40578da494aa0d9239174c4ab
1096490965@qq.com <1096490965@qq.com>
2015-10-08 14:56:12 +0800

    Properties MySQL

---
+++ log-import-db/src/main/java/com/disruptive/utils/PropertiesUtil.java
@@ -0,0 +1,43 @@
+package com.disruptive.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesUtil {
+	
+	private static Properties properties = null;
+
+    public static Properties getInstance() throws IOException {
+        if (properties == null) {
+            properties = new Properties();
+            InputStream resourceAsStream = PropertiesUtil.class.getClassLoader().getResourceAsStream("config.properties");
+            properties.load(resourceAsStream);
+        }
+        return properties;
+    }
+
+    public static String getPropertyString(String propertyName) {
+        String value = null;
+        try {
+            value = getInstance().getProperty(propertyName);
+            if (value == null) {
+                return "";
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return value.toString();
+    }
+
+    public static int getPropertyInt(String propertyName) {
+        int value = 0;
+        try {
+            value = Integer.valueOf(getInstance().getProperty(propertyName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
+
+}
