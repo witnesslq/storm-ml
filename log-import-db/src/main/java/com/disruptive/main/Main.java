8fcf4c7c86c2b020c22a38761dc09139743df59b
1096490965@qq.com <1096490965@qq.com>
2015-10-10 15:39:41 +0800

    Main

--- log-import-db/src/main/java/com/disruptive/main/Main.java
+++ log-import-db/src/main/java/com/disruptive/main/Main.java
@@ -4,6 +4,13 @@ public class Main {
 	
 	public static void main(String[] args){
 		
+		String hdfs=args[0];
+		String yyyyMMdd=args[1];
+		MySQL2HDFS(hdfs,yyyyMMdd);
+	}
+	
+	public static void MySQL2HDFS(String hdfs,String yyyyMMdd){
+		
 	}
 
 }
